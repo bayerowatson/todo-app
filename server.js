68406001329546119178d2331177bd6e5020bf6d
bayer <bayerowatson@gmail.com>
2021-10-29 08:59:00 -0400

    prepped for heroku deploy

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 import todoRoutes from './api/todoRoutes.js';
 import { MongoClient } from 'mongodb';
 import TodoDAO from './dao/todoDAO.js';
 
-
+const __dirname = path.resolve();
 
 const app = express();
 dotenv.config({ path: "./config.env" });
@@ -14,6 +15,9 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+
 
 //start server and connect to DB
 const client = new MongoClient(process.env.ATLAS_URI);
@@ -29,3 +33,8 @@ app.listen(port, () => {
 
 //routes
 app.use(todoRoutes);
+
+//send client index.html
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
